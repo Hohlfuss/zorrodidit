4d7322dc4e87b6e3fb0c321a006168bba612c78d
Jari Rautavuori <jari.rautavuori@gmail.com>
2026-07-03 04:09:00 +0300

    register

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from "express";
+import path from "path";
 
 const app = express();
 const port = process.env.PORT ||3_000;
@@ -6,6 +7,10 @@ const port = process.env.PORT ||3_000;
 app.get("/", (req: Request, res: Response) => {
     res.send("typescript express serveri");
 });
+
+app.get("./register.html", (req: request, res: Response) => {
+    res.send(path.join(__dirname, "../src/register.html"));
+})
 
 app.listen(port, () => {
     console.log(`Serveri pystyssä portissa ${port}`);
