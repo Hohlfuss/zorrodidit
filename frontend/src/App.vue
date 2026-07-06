6701743698778fcd9b2436b49fab46a72ea09a6f
Jari Rautavuori <jari.rautavuori@gmail.com>
2026-07-06 05:40:11 +0300

    bg imagee7 joko vittu ny

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-  <div class="min-h-screen bg-[url('./taustakuva_white.png')] bg-cover bg-center bg-fixed bg-no-repeat">
+  <div class="min-h-screen bg-slate-950 bg-[url('./taustakuva_white.png')] bg-cover bg-center bg-fixed bg-no-repeat">
 
     <router-view />
 
