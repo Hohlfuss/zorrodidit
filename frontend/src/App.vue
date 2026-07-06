abe616c257b7284e9bf961c8b971e3f8e7bb813d
Jari Rautavuori <jari.rautavuori@gmail.com>
2026-07-06 05:04:39 +0300

    bg imagee2

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,9 +2,9 @@
 </script>
 
 <template>
-  <div class="min-h-screen bg-[url('./assets/taustakuva-transparent.png')] bg-cover bg-center bg-fixed bg-no-repeat">
+  <div class="min-h-screen bg-[url('./taustakuva-transparent.png')] bg-cover bg-center bg-fixed bg-no-repeat">
 
     <router-view />
-    
+
   </div>
 </template>
